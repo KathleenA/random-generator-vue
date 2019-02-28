dcaaa93cbd7bb72841b8451c8b73a9dda20ab0b1
Kathleen Auerbach <k.auerbach@stroeermediabrands.de>
2019-02-28 11:22:40 +0100

    add backend service

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,12 +10,27 @@
 // @ is an alias to /src
 import RandomText from '@/components/RandomText.vue'
 import Buttons from '@/components/Buttons.vue'
+import PostsService from '@/services/PostsService'
 
 export default {
   name: 'home',
   components: {
     RandomText,
     Buttons,
+  },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getTipp()
+  },
+  methods: {
+    async getTipp () {
+      const response = await PostsService.fetchTipp()
+      this.tipp = response.data
+    }
   }
 }
 </script>
